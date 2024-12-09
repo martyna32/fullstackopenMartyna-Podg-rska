3950e6acf0b5da81e36f6b5698edfa24a4726798
martyncia <martynciahotuwa@gmail.com>
2024-12-09 14:24:00 +0100

    s

--- part1.1-1.2/src/App.jsx
+++ part1.1-1.2/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
   const course = 'Half Stack application development';
   const parts = [
     { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 8 },
+    { name: 'Using props to pass data', exercises: 7 },
     { name: 'State of a component', exercises: 14 },
   ];
 
